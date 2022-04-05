ab0c753645bf96ed3ea047928108dfc529083f8b
yadunandan-rgb <69295090+yadunandan-rgb@users.noreply.github.com>
2022-04-05 16:18:41 +0530

    changes

---
+++ src/Main/Java/DesignPatterns/ChainOfResponsibility/HiringManager.java
@@ -0,0 +1,9 @@
+package Main.Java.DesignPatterns.ChainOfResponsibility;
+
+public class HiringManager extends Managers{
+
+    @Override
+    protected void ProcessSalary(int employeeSalary) {
+
+    }
+}
