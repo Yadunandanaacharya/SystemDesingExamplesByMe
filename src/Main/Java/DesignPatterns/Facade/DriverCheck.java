1752511b02d818a51f2d9cb65340a7f219e72a7c
yadunandan-rgb <69295090+yadunandan-rgb@users.noreply.github.com>
2022-04-05 10:25:25 +0530

    facade dessing
    example:switch on laptop, when switching on it checks many software and hardware components if all of them are on then switches on the laptop.

---
+++ src/Main/Java/DesignPatterns/Facade/DriverCheck.java
@@ -0,0 +1,8 @@
+package Main.Java.DesignPatterns.Facade;
+
+public class DriverCheck {
+    boolean checkDriverOnBoot() {
+        System.out.println("Checking Driver  on boot");
+        return  true;
+    }
+}
---
+++ src/Main/Java/DesignPatterns/Facade/LaptopMain.java
@@ -0,0 +1,8 @@
+package Main.Java.DesignPatterns.Facade;
+
+public class LaptopMain {
+    public static void main(String[] args) {
+        LaptopOnSwitch laptopOnSwitch = new LaptopOnSwitch();
+        laptopOnSwitch.SwitchOnLaptop();
+    }
+}
